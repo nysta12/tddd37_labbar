f8a99ffe105a7f7ad41a03ea11036806bdcbc478
vicny135 <vicny135@tlvm-4-4-3.ad.liu.se>
2024-12-09 14:45:55 +0100

    Fixed foreign keys and spelling issues

--- lab4/4c/4c.sql
+++ lab4/4c/4c.sql
@@ -1,33 +1,34 @@
 /* ------ Drop tables ------*/
-DROP TABLE IF EXISTS Reservation CASCADE;
-DROP TABLE IF EXISTS Booking CASCADE;
-DROP TABLE IF EXISTS Payment CASCADE;
-DROP TABLE IF EXISTS Passenger CASCADE;
-DROP TABLE IF EXISTS Has_ticket CASCADE;
-DROP TABLE IF EXISTS Contact CASCADE;
-DROP TABLE IF EXISTS Flight CASCADE;
-DROP TABLE IF EXISTS Weekly_schedule CASCADE;
-DROP TABLE IF EXISTS Route CASCADE;
-DROP TABLE IF EXISTS Airport CASCADE;
-DROP TABLE IF EXISTS Has_route CASCADE;
-DROP TABLE IF EXISTS Year CASCADE;
-DROP TABLE IF EXISTS Day CASCADE;
-DROP TABLE IF EXISTS Reserved_passengers CASCADE;
+drop table if exists Reservation cascade;
+drop table if exists Booking cascade;
+drop table if exists Payment cascade;
+drop table if exists Passenger cascade;
+drop table if exists Has_ticket cascade;
+drop table if exists Contact cascade;
+drop table if exists Flight cascade;
+drop table if exists Weekly_schedule cascade;
+drop table if exists Route cascade;
+drop table if exists Airport cascade;
+drop table if exists Has_route cascade;
+drop table if exists Year cascade;
+drop table if exists Day cascade;
+drop table if exists Reserved_passengers cascade;
 
-/* ------ Drop procedures ------*/
-DROP PROCEDURE IF EXISTS addYear;
-DROP PROCEDURE IF EXISTS addDay;
-DROP PROCEDURE IF EXISTS addDestination;
-DROP PROCEDURE IF EXISTS addRoute;
-DROP PROCEDURE IF EXISTS addFlight;
-DROP PROCEDURE IF EXISTS addReservation;
-DROP PROCEDURE IF EXISTS addPassenger;
-DROP PROCEDURE IF EXISTS addContact;
-DROP PROCEDURE IF EXISTS addPayment;
+
+/* ------ Drop procedures ------ */
+drop procedure if exists addYear;
+drop procedure if exists addDay;
+drop procedure if exists addDestination;
+drop procedure if exists addRoute;
+drop procedure if exists addFlight;
+drop procedure if exists addReservation;
+drop procedure if exists addPassenger;
+drop procedure if exists addContact;
+drop procedure if exists addPayment;
 
 /* ------ Drop functions ------*/
-DROP FUNCTION IF EXISTS calculateFreeSeats;
-DROP FUNCTION IF EXISTS calculatePrice;
+drop function if exists calculateFreeSeats;
+drop function if exists calculatePrice;
 
 
 
@@ -37,22 +38,14 @@ reservation_number int,
 flight int,
 contact int,
 constraint pk_reservation
-primary key(reservation_number),
-constraint fk_reservation_contact
-foreign key (contact) references Contact (passport_number),
-constraint fk_reservation_flight
-foreign key (flight) references Fight (flight_number));
+primary key(reservation_number));
     
 create table Booking(
 reservation_number int,
 paid_price double,
 person_paying int,
 constraint pk_booking
-primary key(reservation_number),
-constraint fk_booking_person_paying
-foreign key (person_paying) references Payment (payment_id),
-constraint fk_booking_reservation_number
-foreign key (reservation_number) references Reservation (reservation_number));
+primary key(reservation_number));
     
 create table Payment(
 payment_id int not null auto_increment,
@@ -65,35 +58,28 @@ create table Passenger(
 passport_number int,
 passenger_name varchar(30),
 constraint pk_passenger
-primary key(passportnumber));
+primary key(passport_number));
 
 create table Has_ticket(
 ticket_number int not null,
 booking int,
 passenger int,
 constraint pk_has_ticket
-primary key(booking, passenger),
-constraint fk_has_ticket_booking
-foreign key (booking) references Booking (reservation_number),
-constraint fk_has_ticket_passenger
-foreign key (passenger) references Passenger (passport_number));
+primary key(booking, passenger));
     
 create table Contact(
 phone_number bigint,
 passport_number int,
 email varchar(30),
 constraint pk_contact
-primary key (passport_number),
-constraint fk_contact_passport_number
-foreign key (passport_number) references Passenger (passport_number));
+primary key (passport_number));
     
 create table Flight(
 flight_number int auto_increment,
-week int,
+week_id int,
+week_number int,
 constraint pk_flight
-primary key (flight_number),
-constraint fk_flight_week
-foreign key (week) references Weekly_schedule (week_id));
+primary key (flight_number));
 
 create table Weekly_schedule(
 week_id int not null auto_increment,
@@ -101,11 +87,7 @@ departure_time time,
 route int,
 day varchar(10),
 constraint pk_weekly_schedule
-primary key (week_id),
-constraint fk_weekly_schedule_route
-foreign key (route) references Route (route_id),
-constraint fk_weekly_schedule_day
-foreign key (day) references Day (day_id));
+primary key (week_id));
 
 create table Route(
 route_id int not null auto_increment,
@@ -114,11 +96,7 @@ arrival varchar(3),
 year_id int,
 routeprice double,
 constraint pk_route
-primary key (route_id),
-constraint fk_route_departure
-foreign key (departure) references Airport (airport_code),
-constraint fk_route_arrival
-foreign key (arrival) references Airport (airport_code));
+primary key (route_id));
     
 create table Airport(
 airport_code varchar(3),
@@ -132,11 +110,7 @@ route int not null auto_increment,
 year int,
 route_price double,
 constraint pk_has_route
-primary key (route, year),
-constraint fk_has_route_route
-foreign key (route) references Route (route_id),
-constraint fk_has_route_year
-foreign key (year) references Year (year_id));
+primary key (route, year));
 
 create table Year(
 year_id int,
@@ -149,18 +123,40 @@ day_id varchar(10),
 year int,
 pricing_factor double,
 constraint pk_day
-primary key (day, year));
+primary key (day_id, year));
     
 create table Reserved_passengers(
 passport_number int,
 reservation_number int,
 constraint pk_reserved_passengers
-primary key (passport_number, reservation_number),
-constraint fk_reserved_passengers_reservation_number
-foreign key (reservation_number) references Reservation (reservation_number),
-constraint fk_reserved_passengers_passport_number
-foreign key (passport_number) references Passenger (passport_number));
+primary key (passport_number, reservation_number));
 
+
+/* ------ Add the foreign keys ------ */
+alter table Reservation add constraint fk_reservation_contact foreign key (contact) references Contact (passport_number);
+alter table Reservation add constraint fk_reservation_flight foreign key (flight) references Flight (flight_number);
+
+alter table Booking add constraint fk_booking_person_paying foreign key (person_paying) references Payment (payment_id);
+alter table Booking add constraint fk_booking_reservation_number foreign key (reservation_number) references Reservation (reservation_number);
+
+alter table Has_ticket add constraint fk_has_ticket_booking foreign key (booking) references Booking (reservation_number);
+alter table Has_ticket add constraint fk_has_ticket_passenger foreign key (passenger) references Passenger (passport_number);
+
+alter table Contact add constraint fk_contact_passport_number foreign key (passport_number) references Passenger (passport_number);
+
+alter table Flight add constraint fk_flight_week foreign key (week_id) references Weekly_schedule (week_id);
+
+alter table Weekly_schedule add constraint fk_weekly_schedule_route foreign key (route) references Route (route_id);
+alter table Weekly_schedule add constraint fk_weekly_schedule_day foreign key (day) references Day (day_id);
+
+alter table Route add constraint fk_route_departure foreign key (departure) references Airport (airport_code);
+alter table Route add constraint fk_route_arrival foreign key (arrival) references Airport (airport_code);
+
+alter table Has_route add constraint fk_has_route_route foreign key (route) references Route (route_id);
+alter table Has_route add constraint fk_has_route_year foreign key (year) references Year (year_id);
+
+alter table Reserved_passengers add constraint fk_reserved_passengers_reservation_number foreign key (reservation_number) references Reservation (reservation_number);
+alter table Reserved_passengers add constraint fk_reserved_passengers_passport_number foreign key (passport_number) references Passenger (passport_number);
 
 
 /* ------ Create procedures ------ */
@@ -203,7 +199,22 @@ delimiter;
 delimiter //
 create procedure addFlight (in departure_airport_code varchar(3), in arrival_airport_code varchar(3), in year int, in day varchar(30), in departuretime time)
 begin
+	declare week_nr int;
+    set week_nr = 1;
     
+    insert into Weekly_schedule(week_id, departure_time, route, day)
+    values (day, departuretime,
+    (select Route.route_id from Route
+    where departure_airport_code = Route.departure and arrival_airport_code = Route.arrival and year = Route.year_id));
+    
+    loop
+		insert into Flight values (NULL, week_nr, (select max(id) from weeklyschedule));
+		if week_nr > 52 then
+			leave;
+		end if;
+        set week_nr = week_nr + 1
+	end loop;
+
 end;
 //
 delimiter;
@@ -233,7 +244,15 @@ delimiter ;
 
 
 
+/* ------ Question 8 ------ */
 
+
+
+/* ------ Question 9 ------ */
+
+
+
+/* ------ Question 10 ------ */
 
 
 
